3b56184b3c2d1751fadba71b9be29fdce1794fdd
Kevin Kingslin <kevinkingslin@gmail.com>
2022-07-30 05:56:39 +0530

    Added zoom in feature

--- test.java
+++ test.java
@@ -3,12 +3,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class test extends Applet implements ActionListener {
+	private Button ZoomIn = new Button("Zoom In");
+	private Button ZoomOut = new Button("Zoom Out");
+	private Integer unit = 40;
+
 	public void init() {
 		this.setSize(600, 600);
-		Button ZoomIn = new Button("Zoom in");
-		add(ZoomIn);
 
-		Button ZoomOut = new Button("Zoom Out");
+		add(ZoomIn);
 		add(ZoomOut);
 
 		ZoomIn.addActionListener(this);
@@ -17,55 +19,55 @@ public class test extends Applet implements ActionListener {
 
 	public void paint(Graphics g) {
 
+		int height = getHeight();
+		int width = getWidth();
+
 		// Set origin
-		int originX=(getX()+getWidth())/2;
-		int originY=(getY()+getHeight())/2;
-		
-		int unit = 40;
-		int count = 50;
+		int originX = (getX() + width) / 2;
+		int originY = (getY() + height) / 2;
 
 		g.setColor(Color.red);
 		Font stringFont = new Font("Arial", 4, 18);
 		g.setFont(stringFont);
-		
+
 		// Set x-axis
-		g.drawLine(0,getHeight()/2,getWidth(),getHeight()/2);
-		
+		g.drawLine(0, height / 2, width, height / 2);
+
 		// Set y-axis
-		g.drawLine(getWidth()/2,0,getWidth()/2,getHeight());
-		
+		g.drawLine(width / 2, 0, width / 2, height);
+
 		g.setColor(Color.black);
 		g.drawString("0", originX, originY);
 
-		int positive = 1;
-		int negative = -1;
+		int coordinate = 1;
 
-		while(count >= 0){
-			// Set x
-			g.drawLine(getWidth()/2 + unit,0,getWidth()/2 + unit,getHeight());
-			g.drawLine(getWidth()/2 - unit,0,getWidth()/2 - unit,getHeight());
+		for (int count = 1; count <= 50; count++, coordinate++) {
+			// Set horizontal lines
+			g.drawLine(width / 2 + (unit * count), 0, width / 2 + (unit * count), height);
+			g.drawLine(width / 2 - (unit * count), 0, width / 2 - (unit * count), height);
 
-			// Set y
-			g.drawLine(0,getHeight()/2 + unit,getWidth(),getHeight()/2 + unit);
-			g.drawLine(0,getHeight()/2 - unit,getWidth(),getHeight()/2 - unit);
+			// Set vertical lines
+			g.drawLine(0, height / 2 + (unit * count), width, height / 2 + (unit * count));
+			g.drawLine(0, height / 2 - (unit * count), width, height / 2 - (unit * count));
 
 			// Set x-axis coordinates
-			g.drawString(String.valueOf(positive), originX + unit, originY);
-			g.drawString(String.valueOf(negative), originX, originY + unit);
+			g.drawString(String.valueOf(coordinate), originX + (unit * count), originY);
+			g.drawString("-" + String.valueOf(coordinate), originX, originY + (unit * count));
 
 			// Set y-axis coordinates
-			g.drawString(String.valueOf(positive), originX, originY - unit);
-			g.drawString(String.valueOf(negative), originX - unit, originY);
-
-			unit += 40;
-			positive++;
-			negative--;
-			count--;
+			g.drawString(String.valueOf(coordinate), originX, originY - (unit * count));
+			g.drawString("-" + String.valueOf(coordinate), originX - (unit * count), originY);
 		}
 	}
 
-	public void actionPerformed(ActionEvent e){
-		System.out.println("Button 1 was pressed");
-		// if (e.getSource() == ){}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == ZoomIn) {
+			Graphics g = getGraphics();
+			g.clearRect(0, 0, getWidth(), getHeight());
+			unit += 20;
+			paint(g);
+		} else {
+
+		}
 	}
 }
